260fe7bfebf64b98d60f8e31382b136ae62d8481
Tomas Buzeta <registom123@gmail.com>
2021-12-20 00:21:22 -0300

    addloc and form

--- src/components/Locations/AddLocations/AddLocation.jsx
+++ src/components/Locations/AddLocations/AddLocation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Form from "./presentational/Form"
+import Form from "./presentation/Form"
 const AddLocation = () => {
     return (
         <article className="bg">
